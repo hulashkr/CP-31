c798bec7e2f94351ddd6ba637773d6b95e940b4b
Hulash Kumar <hulashkrcps@gmail.com>
2025-10-12 20:04:17 +0530

    solve A_Game_with_Integers

---
+++ A_Game_with_Integers.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int tt;
+    cin>>tt;
+    while(tt--) {
+        int n;
+        cin>>n;
+        if(n%3==0) cout<<"Second\n";
+        else cout<<"First\n";
+    }
+}
